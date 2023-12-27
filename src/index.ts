fe32268c5392917edcdc8f2f2554be63d3373432
Subin Abid <io@sabid.in>
2023-12-27 16:03:06 +0530

    MVP

--- src/index.ts
+++ src/index.ts
@@ -1,172 +1,45 @@
-export type Row = "A" | "B" | "C" | "D" | "E" | "F" | "G" | "H" | "I" | "J";
-export type Column = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
-export type Direction = "Horizontal" | "Vertical";
-export interface Position {
-  x: Row;
-  y: Column;
-}
-type ShipCellState = "Healthy" | "Hit";
-type GridState = "Empty" | "Miss" | Ship;
+import { createServer } from "net";
+import { Gameboard } from "./game";
 
-export type Request = {
-  position: Position;
-  // Board or GameID eventually
-};
+const game = new Gameboard([]);
 
-export type Response = "Miss" | "Hit" | "Sunk";
+const main = () => {
+  var server = createServer((socket) => {
+    console.log("Client connected");
+    socket.write("Hello\r\n");
+    const gameStatus = "GameStarted";
 
-const validatePosition = (
-  { x, y }: Position, // start
-  direction: Direction,
-  length: number
-): Array<Position> => {
-  if (direction === "Horizontal") {
-    if (y + length > 10) {
-      throw new Error(`Ship is out of bounds: ${x}${y + length}`);
-    }
-  } else {
-    if (x.charCodeAt(0) + length > 74) {
-      throw new Error(
-        `Ship is out of bounds: ${String.fromCharCode(
-          x.charCodeAt(0) + length
-        )}${y}`
-      );
-    }
-  }
+    // Inititate the Ships
+    socket.write("Enter starting position for Carrier\r\n");
 
-  const positions: Array<Position> = [];
-  for (let i = 0; i < length; i++) {
-    if (direction === "Horizontal") {
-      positions.push({ x, y: (y + i) as Column });
-    } else {
-      positions.push({ x: String.fromCharCode(x.charCodeAt(0) + i) as Row, y });
-    }
-  }
-
-  return positions;
-};
-
-type ShipType =
-  | { name: "Carrier"; length: 5 }
-  | { name: "Battleship"; length: 4 }
-  | { name: "Submarine"; length: 3 }
-  | { name: "Cruiser"; length: 3 }
-  | { name: "Destroyer"; length: 2 };
-
-export class Ship {
-  shipType: ShipType;
-  cells: Array<{ position: Position; state: ShipCellState }>;
-
-  constructor(shipType: ShipType, start: Position, direction: Direction) {
-    this.shipType = shipType;
-    this.cells = validatePosition(start, direction, shipType.length).map(
-      (position) => ({ position, state: "Healthy" })
-    );
-  }
-}
-
-// Gameboard
-export class Gameboard {
-  myBoard: Array<Array<GridState>>;
-  theirBoard: Array<Array<Response | "Empty">>;
-
-  constructor(ships: Array<Ship>) {
-    this.myBoard = [];
-    this.theirBoard = [];
-
-    for (let i = 0; i < 10; i++) {
-      this.myBoard[i] = [];
-      this.theirBoard[i] = [];
-      for (let j = 0; j < 10; j++) {
-        this.myBoard[i][j] = "Empty";
-        this.theirBoard[i][j] = "Empty";
+    socket.on("data", (message) => {
+      switch (message.toString().trim()) {
+        case "ping":
+          socket.write("pong\r\n");
+          break;
+        case "board":
+          socket.write(JSON.stringify(game.myBoard));
+          break;
+        case "A1":
+        case "A2":
+        case "A3":
+        case "A4":
+        case "A5":
+          socket.write("Hit\r\n");
+          break;
+        default:
+          console.log("GOT", { raw: message, s: message.toString() });
+          socket.write("Boop\r\n");
+          break;
       }
-    }
 
-    ships.forEach((ship) => {
-      ship.cells.forEach(({ position: { x, y } }) => {
-        const current = this.myBoard[rowToIndex(x)][columntoIndex(y)];
-
-        if (current === "Empty") {
-          this.myBoard[rowToIndex(x)][columntoIndex(y)] = ship;
-        } else {
-          throw new Error(`Ship is overlapping at ${x}${y}`);
-        }
-      });
+      // socket.write("OK\r\n");
     });
-  }
-}
 
-//Convert between index and row/column
-export const indexToRow = (i: number): Row => {
-  if (i < 0 || i > 9) {
-    throw new Error(`Row is out of bounds: ${i}`);
-  }
-  return String.fromCharCode(i + 65) as Row;
+    //  socket.pipe(socket);
+  });
+
+  server.listen(1337, "127.0.0.1");
 };
 
-export const indexToColumn = (i: number): Column => {
-  if (i < 0 || i > 9) {
-    throw new Error(`Column is out of bounds: ${i}`);
-  }
-  return (i + 1) as Column;
-};
-
-export const rowToIndex = (r: Row): number => r.charCodeAt(0) - 65;
-export const columntoIndex = (c: Column): number => c - 1;
-
-// Gameplay
-
-// Attack your opponent
-// export const attack = (gb: Gameboard, { position }: Request) => {
-//   const bytes = JSON.stringify(position);
-//   network.send(bytes);
-//   const responsebytes = network.receive();
-//   const op = JSON.parse(responsebytes) as Response;
-//   gb.theirBoard[rowToIndex(x)][columntoIndex(y)] = op;
-// };
-
-// Uopdate their board after Attack
-export const updateBoardAfterAttack = (
-  gb: Gameboard,
-  { x, y }: Position,
-  response: Response
-) => {
-  gb.theirBoard[rowToIndex(x)][columntoIndex(y)] = response;
-};
-
-// Respond to attack
-export const defend = (
-  gb: Gameboard,
-  { position: { x, y } }: Request
-): Response => {
-  const cell = gb.myBoard[rowToIndex(x)][columntoIndex(y)];
-
-  switch (cell) {
-    case "Empty":
-      gb.myBoard[rowToIndex(x)][columntoIndex(y)] = "Miss";
-      return "Miss";
-
-    case "Miss":
-      return "Miss";
-
-    default:
-      cell.cells.find(
-        (cell) => cell.position.x === x && cell.position.y === y
-      )!.state = "Hit";
-
-      return cell.cells.every((cell) => cell.state === "Hit")
-        ? //
-          "Sunk"
-        : "Hit";
-  }
-};
-
-// const game = new Gameboard([]);
-
-// while (true) {
-//   const bytes = network.receive("Enter x");
-//   const request = parse(bytes) as Request;
-//   const reponse = bombed(game, x.position);
-//   network.send(reponse);
-// }
+main();
